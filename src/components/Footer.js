463c0a1babdbb577ad487b0b7b19d1d1eed2eb3d
Johnny yo <johnnyyo603@gmail.com>
2023-08-14 12:45:00 +0800

    pj8 client all done at 0814,first version

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Footer = () => {
-  return <div className="footer">Wang Johnny 2023.07.18</div>;
+  return <div className="footer"> &copy;Wang Johnny 2023.07.18</div>;
 };
 
 export default Footer;
